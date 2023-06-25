222d65edacd701e8641235952d370216b1f6c4ab
advicebd13579 <136640670+advicebd13579@users.noreply.github.com>
2023-06-25 13:46:23 +0700

    Refine user experience

---
+++ lion_island_ocean.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+# This program will print "See the World!"
+
+use strict;
+use warnings;
+
+# This will print out the string
+print "See the World!\n";
+
+# This while loop will repeat the string
+my $i = 0;
+while ($i < 2000) {
+	print "See the World!\n";
+	$i++;
+}
